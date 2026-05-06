0b83fdc0b419694985d38a3a5d4785c9a3d3f9d8
domaversano <domaversano@users.noreply.github.com>
2026-05-06 13:31:50 +0200

    Version 2
    Improved interface. Addition of Sequencer. Play/Pause button.

--- sketch.js
+++ sketch.js
@@ -1,45 +1,114 @@
-let canvasX = 1200;
-let canvasY = 600;
+let matras = 36; // selects the shape
+let animateSpeed = 0.01; // Speed the animation goes at
+let animatePos = 0; // Position of the animation
+let startingMatra = 36;
+let canvasX = 1250;
+let canvasY = 700;
 let rows = [];
 let cols = [];
-let cells = 10; // the x and the y value of cells
-let gridDensity = 40; // the higher the number the more compact the grid
-let gridSize = 60;
-let circleSize = 24;
-let radius = 260; // radius of the large shape
-let matras = 12; // selects the shape
+let cellX; // x size of grid shapes
+let cellY; // y size of grid shapes
+
+/// Grid
+
+let cells = 10; // number of cells for x and y
+let gridDensity = 40; // the HIGHER the number the more compact the grid
+let circleSize = 24; // size of the circles on the grid
+let gridText = 9; // text size
+let gridXpadding = 60; // X padding for Grid
+let gridYpadding = 60; // Y padding for Grid
+let numberColours = "#D20909";
+let rowGap = 1.05;
+
+// Large Georhythm
+
+let animate = 0; /// If animate = 0 then no animation if animate 0 = 1 then animate.
+let radius = 280; // radius of the large shape
 let textGap = 8.5; /// Gap between numbers
-let highlight = 0; /// the shapes' highlight colour
-let colourSelect; // selects which shapes to highlight
-let cellX;
-let cellY;
-let numbers = 0;
 let numberDist = 1.04;
+let shapeText = 10;
+let geo;
+let speed = 0.00333; // Speed of animation // Radius of large shape
+let shapeThickness = 1; /// Thickness of the shape's line
+let pointSize = 4; /// Thickness of the shape's points
+let animationWeight = 6; /// Thickness of the animated points
+let rhythms = [];
+
+/// Roll
+
+let divs = [];
+let placeX = 10;
+let placeY = 690;
+let rollX = 1200;
+let rollY = 250;
+let xPad = 0;
+let yPad = 45;
+let lineSpacing = 12; // horizontal gap between lines
+let lineHeight = 150; // total height of roll
+let lineWeight = 0.2;
+let polyWeight = 1.4;
+let prune = 8;
+let xAdj = -20;
+let yAdj = 10;
+
+/// Toggle
+
+let toggleSize = 20;
+let toggleState = 0;
+let toggleX = canvasX - 615;
+let toggleY = 52;
 
 function setup() {
-  createCanvas(canvasX, canvasY);
+  createCanvas(canvasX, canvasY + 200);
   cellX = (canvasX - gridDensity * 2) / (cells * 2);
   cellY = (canvasY - gridDensity * 2) / cells;
   for (let i = 0; i < cells; i++) {
     rows[i] = cellX * i;
-    cols[i] = cellY * i;
-    print(rows[i]);
+    cols[i] = cellY * i * rowGap;
+  }
+
+  /// Start with This Georhythm
+
+  for (let m = 0; m < startingMatra; m++) {
+    if (matras % (m + 1) === 0) {
+      let geo = new Shape(
+        m + 1,
+        radius,
+        speed,
+        shapeThickness,
+        animationWeight,
+        pointSize
+      );
+      rhythms.push(geo);
+    }
+  }
+
+  /// Load the first roll
+
+  for (let rhy = 0; rhy < matras; rhy++) {
+    if (matras % (rhy + 1) === 0) {
+      divs.push(rhy + 1);
+    }
   }
 }
 
 function draw() {
-  // Draw grid
+  /// DRAW GRID
+
+  clear();
+  background("#E3E0DD"); /// overall colour
+  animatePos = animatePos + speed;
   push();
-  translate(60, 70);
-  background(255, 195, 25, 30);
-  numbers = 0;
+  translate(gridXpadding, gridYpadding); /// Position of the grid
+
   for (let i = 0; i < cells; i++) {
     for (let j = 0; j < cells; j++) {
       let matra = i * cells + (j + 1);
       push();
       translate(rows[j], cols[i]);
-      textSize(9);
-      text(matra, -gridSize / 2, -gridSize / 2 + 7);
+      textSize(gridText);
+      fill(numberColours); /// color of the numbers
+      text(matra, -cellX / 2 - 3, -cellY / 2 + 7);
       rotate(PI * 1.5);
 
       for (let a = 0; a < matra; a++) {
@@ -47,17 +116,9 @@ function draw() {
           beginShape();
           noFill();
 
-          if (a === colourSelect) {
-            stroke(highlight);
-            strokeWeight(2.5);
-          } else {
-            stroke(0);
-            strokeWeight(1);
-          }
-
-          for (let i = 0; i < PI * 2; i += (PI * 2) / (a + 1)) {
-            let vx = cos(i) * circleSize;
-            let vy = sin(i) * circleSize;
+          for (let ang = 0; ang < PI * 2; ang += (PI * 2) / (a + 1)) {
+            let vx = cos(ang) * circleSize;
+            let vy = sin(ang) * circleSize;
             vertex(vx, vy);
           }
           endShape(CLOSE);
@@ -68,81 +129,129 @@ function draw() {
   }
   pop();
 
-  // Draws the Large Shape on the right
+  // DRAW LARGE SHAPE
 
-  textPosition = -305;
-  translate(600, 0);
-  textSize(10);
-  text("Time cycle", 4, 50);
-  text(matras, 4, 60.5);
-  text("Rhythms ", 4, 548);
-  translate(300, 300);
+  push();
+
+  translate(canvasX / 1.33333, canvasY / 2);
+  textPosition = 672;
+  textSize(shapeText);
+  fill('#036AC8')
+  text("Time cycle", -300, -300);
+  text(matras, -250, -300);
+  text("Rhythms ", -300, 310);
   rotate(PI * 1.5);
+
+  for (let geo of rhythms) {
+    geo.show();
+
+    if (toggleState === 1) {
+      geo.animate();
+    }
+  }
+
+  pop();
 
   for (let a = 0; a < matras; a++) {
     if (matras % (a + 1) === 0) {
       let division = a + 1;
       push();
-      rotate(PI * 0.5);
-      textSize(10);
-      text(division, textPosition + textGap, 260);
+      rotate(PI * 2);
+      textSize(10); /// CREATE A VARIABLE FOR THIS
+      fill('#036AC8');
+      text(division, textPosition + textGap, 660);
       if (division < 10) {
         textPosition = textPosition + textGap;
       } else {
         textPosition = textPosition + (textGap + 7);
       }
       pop();
-
-      push();
-      beginShape();
-      noFill();
-      if (a === colourSelect) {
-        stroke(highlight);
-        strokeWeight(2.5);
-      } else {
-        stroke(0);
-        strokeWeight(1);
-      }
-      for (let i = 0; i < PI * 2; i += (PI * 2) / (a + 1)) {
-        x = cos(i) * radius;
-        y = sin(i) * radius;
-        stroke(0 + a);
-        vertex(x, y);
-      }
-      endShape(CLOSE);
-      pop();
     }
   }
 
-  // Draws the black dots on the shape
+  /// Draw Roll
+
+  let rollPos;
+
+  rollPos = floor(map(animatePos % 1, 0, 1, 0, matras));
 
   push();
-  beginShape();
+  translate(placeX, placeY);
 
-  for (let angle = 0; angle < PI * 2 - 0.01; angle += (PI * 2) / matras) {
-    numbers = numbers + 1;
-    let x = cos(angle) * radius;
-    let y = sin(angle) * radius;
-    strokeWeight(4);
-    stroke(40);
-    point(x, y);
-    strokeWeight(0.1);
-    push();
-    translate(x * numberDist, y * numberDist);
-    rotate(HALF_PI); // 90° clockwise
-    textAlign(CENTER, CENTER);
-    textSize(8);
-    text(numbers, 0, 0);
-    pop();
+  textSize(10);
+  fill('#036AC8')
+  text("Sequence", 17, 9);
+
+  for (let x = 0; x < matras; x++) {
+    // Gridlines
+    strokeWeight(lineWeight);
+    stroke(255, 0, 0);
+    line(
+      xPad + lineSpacing * x + xPad / 2 - xAdj,
+      yPad,
+      xPad + lineSpacing * x + xPad / 2 - xAdj,
+      yPad + lineHeight
+    );
+    noStroke();
+    fill("#D20909");
+    textSize(9);
+    text(x + 1, xPad + lineSpacing * x + xPad / 2 - 3 - xAdj, 27);
   }
-  endShape(CLOSE);
+
+  for (let a = 0; a < divs.length; a++) {
+    for (let b = 0; b < matras / divs[a]; b++) {
+      strokeWeight(polyWeight); /// Search
+
+      if (toggleState === 1) {
+        if (b * divs[a] === rollPos) {
+          stroke("#036AC8");
+          strokeWeight(polyWeight * 2);
+        } else {
+          stroke(0);
+          strokeWeight(polyWeight);
+        }
+      } else {
+        stroke(0);
+      }
+
+      line(
+        xPad + xPad / 2 + lineSpacing * (divs[a] * b) - xAdj,
+        yPad + lineHeight - (lineHeight * a) / divs.length,
+        xPad + xPad / 2 + lineSpacing * (divs[a] * b) - xAdj,
+        yPad + lineHeight - (lineHeight * (a + 1)) / divs.length + prune
+      );
+    }
+  }
+
+  pop();
+
+  /// Toggle
+
+  push();
+  translate(toggleX+3, toggleY+2);
+  noFill();
+  stroke("#036AC8");
+  strokeWeight(0.7);
+  circle(8.5, 10, 18);
+  let frag = toggleSize / 5; /// fragment
+
+  if (toggleState === 0) {
+    noStroke();
+    fill("#036AC8");
+    triangle(frag, frag, frag, frag * 4, frag * 4, toggleSize / 2);
+  } else {
+    noStroke();
+    fill("#036AC8");
+    rect((frag * 1.5)-1.5, frag * 1.5, frag * 2, frag * 2);
+  }
   pop();
 }
+
 function mousePressed() {
   for (let j = 0; j < cells; j++) {
     for (let i = 0; i < cells; i++) {
-      let x = 60 + rows[j];
-      let y = 70 + cols[i];
+      let x = gridXpadding + rows[j];
+      let y = gridYpadding + cols[i];
 
       let dis = dist(mouseX, mouseY, x, y);
 
@@ -151,27 +260,120 @@ function mousePressed() {
       }
     }
   }
+
+  rhythms = [];
+
+  for (let m = 0; m < matras; m++) {
+    if (matras % (m + 1) === 0) {
+      let geo = new Shape(
+        m + 1,
+        radius,
+        speed,
+        shapeThickness,
+        animationWeight,
+        pointSize
+      );
+      rhythms.push(geo);
+    }
+  }
+
+  divs = [];
+
+  for (let rhy = 0; rhy < matras; rhy++) {
+    if (matras % (rhy + 1) === 0) {
+      divs.push(rhy + 1);
+    }
+  }
+
+  /// Play/Stop Toggle
+
+  animatePos = 0;
+
+  let tx = toggleX + toggleSize / 2;
+  let ty = toggleY + toggleSize / 2;
+  let tDist = dist(mouseX, mouseY, tx, ty);
+
+  print(tDist);
+  print("hello");
+
+  if (tDist < toggleSize / 2) {
+    if (toggleState == 0) {
+      toggleState = 1;
+    } else {
+      toggleState = 0;
+    }
+  }
 }
 
-/// Colour selector that prevents the key creating browser interference
+class Shape {
+  constructor(points, radius, speed, shapeWeight, dotWeight, pointWeight) {
+    this.p = points;
+    this.r = radius;
+    this.s = speed;
+    this.w = shapeWeight;
+    this.d = dotWeight;
+    this.pw = pointWeight;
+  }
 
-function keyPressed() {
+  show() {
+    angleMode(RADIANS);
+    let sections = TWO_PI / this.p; /// how many degrees in each section
 
-  if (key === "1") colourSelect = 1;
-  else if (key === "2") colourSelect = 2;
-  else if (key === "3") colourSelect = 3;
-  else if (key === "4") colourSelect = 4;
-  else if (key === "5") colourSelect = 5;
-  else if (key === "6") colourSelect = 6;
-  else if (key === "7") colourSelect = 7;
-  else if (key === "8") colourSelect = 8;
-  else if (key === "9") colourSelect = 9;
-  else if (key === "0") colourSelect = 10;
+    beginShape();
+    noFill();
+    for (let z = 0; z < this.p; z++) {
+      let x = cos(sections * z) * this.r;
+      let y = sin(sections * z) * this.r;
 
-  return false;
+      /// Add shapes and points
+
+      strokeWeight(this.pw);
+      point(x, y);
+      stroke(0);
+      vertex(x, y);
+      strokeWeight(this.w);
+
+      /// Add numbers
+
+      push();
+      translate(x * 1.04, y * 1.04);
+      rotate(PI / 2); // 90° clockwise
+      textAlign(CENTER, CENTER);
+      strokeWeight(0.7);
+      textSize(8);
+      if (this.p === matras) {
+        noStroke();
+        fill("#D20909"); /// Make this not accumulate DARK
+        text(z + 1, 0, 0);
+      }
+      pop();
+    }
+    endShape(CLOSE);
+  }
+
+  animate() {
+    let sections = TWO_PI / this.p; /// how many degrees in each section
+    let place;
+
+    place = animatePos % 1;
+
+    let stage = floor(place * this.p); /// what stage of the shape
+    let stageMove = map(place % (1 / this.p), 0, 1 / this.p, 0, 1); // Is this ok
+
+    let xSec = cos(sections * stage) * this.r;
+    let ySec = sin(sections * stage) * this.r;
+    let xSec2 = cos(sections * (stage + 1)) * this.r;
+    let ySec2 = sin(sections * (stage + 1)) * this.r;
+
+    let xMove = map(stageMove, 0, 1, xSec, xSec2);
+    let yMove = map(stageMove, 0, 1, ySec, ySec2);
+
+    if (this.p == 1) {
+      stroke("#000000");
+    } else {
+      stroke("#036AC8");
+    }
+    strokeWeight(this.d);
+    point(xMove, yMove);
+  }
 }
-
-function keyReleased() {
-  colourSelect = 110;
-}
-
